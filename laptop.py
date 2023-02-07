d7f55eadd3b0c281e53d0558b943ef715a16913a
hauszmanndavid <93594997+hauszmanndavid@users.noreply.github.com>
2023-02-07 08:36:19 +0100

    asd

--- laptop.py
+++ laptop.py
@@ -2,6 +2,13 @@ file = ("gep.txt","r")
 
 Llaptop = []
 
+for i in file:
+    if(i[-1]=='\t'):
+        Llaptop.append(i[:-1].split('\t'))
+    else:
+        Llaptop.append(i.split('\t'))
+  
+del Llaptop[0]
 #1
 
 
@@ -10,7 +17,13 @@ Llaptop = []
 
 
 #3
+
+
+#4
 colos = 0
 
+
+#5
+statisztika = 0
 
 
