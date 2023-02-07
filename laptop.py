e04bf798baa54aef7b4fecee3f861a9255c5c5f5
hauszmanndavid <93594997+hauszmanndavid@users.noreply.github.com>
2023-02-07 09:05:40 +0100

    asd

--- laptop.py
+++ laptop.py
@@ -3,27 +3,40 @@ file = ("gep.txt","r")
 Llaptop = []
 
 for i in file:
-    if(i[-1]=='\t'):
+    if(i[-1]=='\n'):
         Llaptop.append(i[:-1].split('\t'))
     else:
-        Llaptop.append(i.split('\t'))
+        Llaptop.append(i[:-1].split('\t'))
   
 del Llaptop[0]
+
 #1
 
 
 
 #2
+osszeg=0
+for i in range(len(Llaptop)):
+   osszeg += int(Llaptop[i][3])
+
+print("Átlag ennyi merevlemez van a gépeken: ", round(osszeg/len(Llaptop),2))
 
 
 #3
+'''keres = 0
 
+for i in range(Llaptop):
+    if(keres == (Llaptop[i][1])):
+        print("Van 1TB gép")
+    else:
+        print("Nincs 1TB gép")
 
 #4
 colos = 0
 
 
+
 #5
 statisztika = 0
 
-
+'''
